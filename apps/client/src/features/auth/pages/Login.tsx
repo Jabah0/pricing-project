60ec3271362245d9bb829363c43c73a0ba70b65f
jabah <mo.jabah0@gmail.com>
2024-07-27 14:55:14 +0200

    temp commit

--- apps/client/src/features/auth/pages/Login.tsx
+++ apps/client/src/features/auth/pages/Login.tsx
@@ -1,23 +1,28 @@
 import { DarkModeSwitcher } from "@/features/layout/components/DarkModeSwitcher";
-import loginImage from "../../../assets/login.png";
+import { DotLottieSolid } from "@lottiefiles/dotlottie-solid";
 import { LoginForm } from "../components/LoginForm";
 import { LanguageSwitcher } from "@/features/layout/components/LanguageSwitcher";
 
 export const Login = () => {
   return (
     <div class="flex flex-wrap justify-center items-center w-full h-full">
-      <div class="lg:basis-2/3 lg:flex hidden justify-center items-center">
-        <img src={loginImage} class="w-[40rem] sm:w-[30rem]" />
-      </div>
       <div
         class="flex flex-col justify-start items-center gap-20 lg:basis-1/3 bg-backgroundForm 
         w-full h-full shadow-xl px-2"
       >
-        <div class="flex items-start justify-end gap-4 w-full px-2 py-3">
+        <div class="flex items-end justify-start gap-4 w-full px-2 py-3">
           <LanguageSwitcher />
           <DarkModeSwitcher />
         </div>
         <LoginForm />
+      </div>
+
+      <div class="lg:basis-2/3 lg:flex hidden justify-center items-center">
+        <DotLottieSolid
+          src="../../../assets/loginAnimation.json"
+          loop
+          autoplay
+        />
       </div>
     </div>
   );
