23a39a86df68ce33fcad87eb40be3c2e9c018ce0
Mohammad Jabah <jabah@localhost.localdomain>
2024-11-05 20:30:52 +0200

    temp commit

--- apps/client/src/features/layout/components/Sidebar.tsx
+++ apps/client/src/features/layout/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { ParentComponent } from "solid-js";
 export const Sidebar: ParentComponent = (props) => {
   return (
     <div
-      class="hidden bg-backPrimary w-[6rem] rounded-r-sm 
+      class="hidden bg-backPrimary w-[3.5rem] rounded-r-sm 
       shadow-xl py-6 xl:flex lg:flex h-full overflow-auto"
     >
       {props.children}
